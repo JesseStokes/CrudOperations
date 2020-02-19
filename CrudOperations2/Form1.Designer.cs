de1b47bc6a2330b828643493b483f15233fa3892
JesseStokes <53128286+JesseStokes@users.noreply.github.com>
2020-02-19 17:37:12 -0600

    Add files via upload

---
+++ CrudOperations2/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace CrudOperations2
+{
+    partial class CRUD_Operations
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.lbLastName = new System.Windows.Forms.Label();
+            this.lbRollNumber = new System.Windows.Forms.Label();
+            this.lbAddress = new System.Windows.Forms.Label();
+            this.lbPhone = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbRollNumber = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.btInsert = new System.Windows.Forms.Button();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.StudentRecordDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentRecordDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFirstName.Location = new System.Drawing.Point(45, 36);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(76, 16);
+            this.lbFirstName.TabIndex = 0;
+            this.lbFirstName.Text = "First Name:";
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.AutoSize = true;
+            this.lbLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLastName.Location = new System.Drawing.Point(361, 36);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(76, 16);
+            this.lbLastName.TabIndex = 0;
+            this.lbLastName.Text = "Last Name:";
+            // 
+            // lbRollNumber
+            // 
+            this.lbRollNumber.AutoSize = true;
+            this.lbRollNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRollNumber.Location = new System.Drawing.Point(45, 101);
+            this.lbRollNumber.Name = "lbRollNumber";
+            this.lbRollNumber.Size = new System.Drawing.Size(86, 16);
+            this.lbRollNumber.TabIndex = 0;
+            this.lbRollNumber.Text = "Roll Number:";
+            // 
+            // lbAddress
+            // 
+            this.lbAddress.AutoSize = true;
+            this.lbAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAddress.Location = new System.Drawing.Point(361, 101);
+            this.lbAddress.Name = "lbAddress";
+            this.lbAddress.Size = new System.Drawing.Size(62, 16);
+            this.lbAddress.TabIndex = 0;
+            this.lbAddress.Text = "Address:";
+            // 
+            // lbPhone
+            // 
+            this.lbPhone.AutoSize = true;
+            this.lbPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPhone.Location = new System.Drawing.Point(45, 166);
+            this.lbPhone.Name = "lbPhone";
+            this.lbPhone.Size = new System.Drawing.Size(50, 16);
+            this.lbPhone.TabIndex = 0;
+            this.lbPhone.Text = "Phone:";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFirstName.Location = new System.Drawing.Point(155, 33);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(176, 22);
+            this.tbFirstName.TabIndex = 1;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLastName.Location = new System.Drawing.Point(443, 33);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(176, 22);
+            this.tbLastName.TabIndex = 2;
+            // 
+            // tbRollNumber
+            // 
+            this.tbRollNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRollNumber.Location = new System.Drawing.Point(155, 98);
+            this.tbRollNumber.Name = "tbRollNumber";
+            this.tbRollNumber.Size = new System.Drawing.Size(176, 22);
+            this.tbRollNumber.TabIndex = 3;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAddress.Location = new System.Drawing.Point(443, 98);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(176, 22);
+            this.tbAddress.TabIndex = 4;
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPhone.Location = new System.Drawing.Point(155, 163);
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(176, 22);
+            this.tbPhone.TabIndex = 5;
+            // 
+            // btInsert
+            // 
+            this.btInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btInsert.Location = new System.Drawing.Point(49, 211);
+            this.btInsert.Name = "btInsert";
+            this.btInsert.Size = new System.Drawing.Size(90, 23);
+            this.btInsert.TabIndex = 6;
+            this.btInsert.Text = "Insert";
+            this.btInsert.UseVisualStyleBackColor = true;
+            this.btInsert.Click += new System.EventHandler(this.btInsert_Click);
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btUpdate.Location = new System.Drawing.Point(209, 211);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(90, 23);
+            this.btUpdate.TabIndex = 7;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            this.btUpdate.Click += new System.EventHandler(this.btUpdate_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDelete.Location = new System.Drawing.Point(369, 211);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(90, 23);
+            this.btDelete.TabIndex = 8;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btClear.Location = new System.Drawing.Point(529, 211);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(90, 23);
+            this.btClear.TabIndex = 9;
+            this.btClear.Text = "Clear";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // StudentRecordDataGridView
+            // 
+            this.StudentRecordDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentRecordDataGridView.Location = new System.Drawing.Point(48, 250);
+            this.StudentRecordDataGridView.Name = "StudentRecordDataGridView";
+            this.StudentRecordDataGridView.Size = new System.Drawing.Size(571, 177);
+            this.StudentRecordDataGridView.TabIndex = 10;
+            this.StudentRecordDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StudentRecordDataGridView_CellClick);
+            // 
+            // CRUD_Operations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 450);
+            this.Controls.Add(this.StudentRecordDataGridView);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.btInsert);
+            this.Controls.Add(this.tbPhone);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbRollNumber);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.lbPhone);
+            this.Controls.Add(this.lbAddress);
+            this.Controls.Add(this.lbRollNumber);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.lbFirstName);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CRUD_Operations";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD Operations 2";
+            this.Load += new System.EventHandler(this.CRUD_Operations_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentRecordDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbFirstName;
+        private System.Windows.Forms.Label lbLastName;
+        private System.Windows.Forms.Label lbRollNumber;
+        private System.Windows.Forms.Label lbAddress;
+        private System.Windows.Forms.Label lbPhone;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbRollNumber;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.Button btInsert;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.DataGridView StudentRecordDataGridView;
+    }
+}
+
---
+++ CrudOperations2/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CrudOperations2
+{
+    public partial class CRUD_Operations : Form
+    {
+        public CRUD_Operations()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source =DESKTOP-71Q98DC;Initial Catalog =CrudOperations2;Integrated Security = True");
+        public int StudentID;
+
+        private void CRUD_Operations_Load(object sender, EventArgs e)
+        {
+            GetStudentsRecord();
+        }
+
+        private void GetStudentsRecord()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Students", con);
+            DataTable dt = new DataTable();
+
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            StudentRecordDataGridView.DataSource = dt;
+        }
+
+        private void btInsert_Click(object sender, EventArgs e)
+        {
+            if (IsValid())
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO Students VALUES (@FirstName, @LastName, @RollNumber, @Address, @Phone)", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@FirstName", tbFirstName.Text);
+                cmd.Parameters.AddWithValue("@LastName", tbLastName.Text);
+                cmd.Parameters.AddWithValue("@RollNumber", tbRollNumber.Text);
+                cmd.Parameters.AddWithValue("@Address", tbAddress.Text);
+                cmd.Parameters.AddWithValue("@Phone", tbPhone.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("New Student is successfully saved in the database", "Entry Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetStudentsRecord();
+                ResetFormControls();
+            }
+        }
+
+        private bool IsValid()
+        {
+            if(tbFirstName.Text == string.Empty)
+            {
+                MessageBox.Show("Student Name is required", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            ResetFormControls();
+        }
+
+        private void ResetFormControls()
+        {
+            StudentID = 0;
+            tbFirstName.Clear();
+            tbLastName.Clear();
+            tbRollNumber.Clear();
+            tbAddress.Clear();
+            tbPhone.Clear();
+
+            tbFirstName.Focus();
+        }
+
+        private void StudentRecordDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            StudentID = Convert.ToInt32(StudentRecordDataGridView.SelectedRows[0].Cells[0].Value);
+            tbFirstName.Text = StudentRecordDataGridView.SelectedRows[0].Cells[1].Value.ToString();
+            tbLastName.Text = StudentRecordDataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            tbRollNumber.Text = StudentRecordDataGridView.SelectedRows[0].Cells[3].Value.ToString();
+            tbAddress.Text = StudentRecordDataGridView.SelectedRows[0].Cells[4].Value.ToString();
+            tbPhone.Text = StudentRecordDataGridView.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+        private void btUpdate_Click(object sender, EventArgs e)
+        {
+            if(StudentID > 0)
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE Students SET FirstName = @FirstName, LastName = @LastName, RollNumber = @RollNumber, Address = @Address," +
+                    " Phone = @Phone WHERE StudentID = @ID", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@FirstName", tbFirstName.Text);
+                cmd.Parameters.AddWithValue("@LastName", tbLastName.Text);
+                cmd.Parameters.AddWithValue("@RollNumber", tbRollNumber.Text);
+                cmd.Parameters.AddWithValue("@Address", tbAddress.Text);
+                cmd.Parameters.AddWithValue("@Phone", tbPhone.Text);
+                cmd.Parameters.AddWithValue("@ID", this.StudentID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Student information is updated", "Updated Successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                GetStudentsRecord();
+                ResetFormControls();
+            }
+            else
+            {
+                MessageBox.Show("Select a student to update their information", "Invalid Selection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            if(StudentID > 0)
+            {
+                SqlCommand cmd = new SqlCommand("DELETE FROM Students WHERE StudentID = @ID", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@ID", this.StudentID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Student is deleted from the system", "Deletion Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                GetStudentsRecord();
+                ResetFormControls();
+            }
+            else
+            {
+                MessageBox.Show("Select a student to delete", "Select Student", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
